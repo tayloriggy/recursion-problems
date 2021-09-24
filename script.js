1de1d35750544098cd4faf9cdb773674c82db3be
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-23 21:57:09 -0500

    update

--- script.js
+++ script.js
@@ -4,5 +4,27 @@ Write a JavaScript function to calculate the factorial of a number
 Steps:
 1. Create a function.
 2. Pass in a number as a parameter.
-3. 
+3. Create a base case that will tell the function when to stop calling itself.
+4. Call the function and tell it what to do when it calls itself.
 */
+
+function findFactorial (num) {
+    if (num === 0) {
+        return 1;
+    }
+    else {
+        return num * findFactorial(num - 1);
+    }
+    
+    /*
+    findFactorial(5) returns 5 * findFactorial(4)
+    findFactorial(4) returns 5 * 4 * findFactorial(3)
+    findFactorial(3) returns 5 * 4 * 3 * findFactorial(2)
+    findFactorial(2) returns 5 * 4 * 3 * 2 * findFactorial(1)
+    findFactorial(1) returns 5 * 4 * 3 * 2 * 1 * findFactorial(0)
+    findFactorial(0) returns 5 * 4 * 3 * 2 * 1 * 1
+    */
+
+}
+
+findFactorial(5);
